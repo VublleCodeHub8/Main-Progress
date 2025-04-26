f86493c487af4b34f3ada02765fcc8b05dbde68f
Himanshu-Saraswat-01122004 <himanshu.s22@iiits.in>
2025-04-27 01:52:16 +0530

    majorchange

--- frontend/src/pages/Wrapper.jsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from "react-router-dom";
-import { useSelector } from "react-redux";
-import Landing from "@/components/Landing";
-import Auth from "./Auth";
-
-export default function Wrapper() {
-  const token = useSelector((state) => state.misc.token);
-  return <>{token.token != null ? <Outlet></Outlet> : <Landing />}</>;
-}
